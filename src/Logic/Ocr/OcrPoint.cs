da33b289039c73222912ed4484eda933398e4c7d
Nikolaj Olsson <nikse.dk@gmail.com>
2024-11-12 20:04:16 +0100

    Move some code from SE

---
+++ src/Logic/Ocr/OcrPoint.cs
@@ -0,0 +1,13 @@
+﻿namespace SubtitleAlchemist.Logic.Ocr;
+
+public struct OcrPoint
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public OcrPoint(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
